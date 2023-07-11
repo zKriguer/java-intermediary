1ae48e27ca6266e62d24cf2a166298841150a41a
zKriguer <kriguerfreitas2020@gmail.com>
2023-07-11 02:17:54 -0300

    exercise: working with generics

---
+++ PrintService/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.print("First " + ps.first());
+
+        sc.close();
+    }
+}
